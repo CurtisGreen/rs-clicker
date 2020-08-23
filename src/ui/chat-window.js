5c849b7cb55d2e1a26cebed8b606cebcc0271b9a
unknown <c.green5201@gmail.com>
2020-08-22 23:45:12 -0500

    Converted chat window into a scroll window

--- src/ui/chat-window.js
+++ src/ui/chat-window.js
@@ -1,27 +1,11 @@
 import { OBJECT_TYPES, CONSTANTS, FONTS } from "../constants/constants.js";
+import { ScrollWindow } from "./scroll-window.js";
+import { TextRow } from "./text-row.js";
 
 export class ChatScene extends Phaser.Scene {
-    // Used for all object types
     chatWindow;
     shopChatWindow;
-    objNameText;
-    objExamineText;
-    welcomeText;
-    playerNameText;
-    row2Header;
-    row2Text;
-
-    // Equipment
-    itemStatHeaders;
-    itemStatLabels = [];
-    itemStatText = {};
-    requiredLevelHeader;
-    requiredLevelText;
-
-    // Enemies
-    enemyStatHeaders;
-    enemyStatLabels = [];
-    enemyStatText = {};
+    scrollWindow;
 
     constructor() {
         super({ key: CONSTANTS.SCENES.CHAT });
@@ -33,6 +17,27 @@ export class ChatScene extends Phaser.Scene {
     }
 
     create() {
+        // Setup scroll window
+        this.scrollWindow = new ScrollWindow("chat");
+        this.scene.add("scroll-window", this.scrollWindow, true);
+        let welcomeText = this.add.text(
+            10,
+            444,
+            "Welcome to RS Clicker",
+            FONTS.ITEM_HEADER
+        );
+        let startArray = [welcomeText];
+
+        this.scrollWindow.addObjects({
+            x: 0,
+            y: 345,
+            width: 495,
+            height: 113,
+            numColumns: 1,
+            padding: 0,
+            objects: startArray,
+        });
+
         // Chat window for examining items
         this.chatWindow = this.add
             .image(0, 338, "chat-window")
@@ -43,231 +48,157 @@ export class ChatScene extends Phaser.Scene {
             .setOrigin(0, 0)
             .setDepth(0);
 
-        // Setup table dimensions
-        let tableStartX = 140;
-        let tableStartY = 380;
-        let tableWidth = 70;
-        let tableHeight = 16;
-
         // General info
-        this.objNameText = this.add.text(10, tableStartY - 32, "", FONTS.ITEM_HEADER);
-        this.objExamineText = this.add.text(
-            tableStartX,
-            tableStartY - 32,
-            "",
-            FONTS.ITEM_STATS
-        );
-
-        // Welcome text
-        this.welcomeText = this.add.text(
-            10,
-            444,
-            "Welcome to RS Clicker",
-            FONTS.ITEM_HEADER
-        );
         this.playerNameText = this.add.text(10, 459, "You", FONTS.ITEM_HEADER);
 
-        //
-        // Items
-        //
-
-        this.row2Header = this.add.text(
-            10,
-            tableStartY - 16,
-            "Sells for:",
-            FONTS.ITEM_HEADER
-        );
-        this.row2Text = this.add.text(
-            tableStartX,
-            tableStartY - 16,
-            "",
-            FONTS.ITEM_STATS
-        );
-
-        //
-        // Equipment
-        //
-
-        // Required equip level
-        this.requiredLevelHeader = this.add.text(
-            10,
-            tableStartY,
-            "Required Level:",
-            FONTS.ITEM_HEADER
-        );
-        this.requiredLevelText = this.add.text(
-            tableStartX,
-            tableStartY,
-            "0",
-            FONTS.ITEM_STATS
-        );
-
-        // Stat headers
-        let text = "Accuracy Bonuses:\nDefense Bonuses:\nDamage Bonuses:";
-        this.itemStatHeaders = this.add.text(
-            10,
-            tableStartY + tableHeight,
-            text,
-            FONTS.ITEM_HEADER
-        );
-
-        // Column labels
-        let labels = [
-            "Stab\nStab\nMelee",
-            "Slash\nSlash\nPrayer",
-            "Crush\nCrush",
-            "Magic\nMagic\nMagic",
-            "Ranged\nRanged\nRanged",
-        ];
-        labels.forEach((header, index) => {
-            this.itemStatLabels[index] = this.add.text(
-                tableStartX + tableWidth * index,
-                tableStartY + tableHeight,
-                header,
-                FONTS.ITEM_STATS
-            );
-        });
-
-        // Variable names in the item class
-        let itemStats = [
-            "stabBonus",
-            "slashBonus",
-            "crushBonus",
-            "magicBonus",
-            "rangedBonus",
-            "stabDefenseBonus",
-            "slashDefenseBonus",
-            "crushDefenseBonus",
-            "magicDefenseBonus",
-            "rangedDefenseBonus",
-            "strengthBonus",
-            "prayerBonus",
-            "",
-            "magicStrengthBonus",
-            "rangedStrengthBonus",
-        ];
-
-        // Create text fields for the stat values
-        itemStats.forEach((varName, index) => {
-            if (varName != "") {
-                let column = index % 5;
-                let row = Math.floor(index / 5);
-                this.itemStatText[varName] = this.add.text(
-                    tableStartX + 50 + tableWidth * column,
-                    tableStartY + tableHeight * (row + 1),
-                    "0",
-                    FONTS.ITEM_STATS
-                );
-            }
-        });
-
-        //
-        // Enemies
-        //
-
-        // Stat headers
-        text = "Stats:\nAccuracy Bonuses:\nDamage Bonuses:\nDefense Bonuses:";
-        this.enemyStatHeaders = this.add.text(10, tableStartY, text, FONTS.ITEM_HEADER);
-
-        // Column labels
-        labels = [
-            "Attack\nMelee\nMelee\nStab",
-            "Strength\n\n\nSlash",
-            "Defense\n\n\nCrush",
-            "Magic\nMagic\nMagic\nMagic",
-            "Ranged\nRanged\nRanged\nMagic",
-        ];
-        labels.forEach((header, index) => {
-            this.enemyStatLabels[index] = this.add.text(
-                tableStartX + tableWidth * index,
-                tableStartY,
-                header,
-                FONTS.ITEM_STATS
-            );
-        });
-
-        // Variable names in the enemy class
-        let enemyStats = [
-            "attack",
-            "strength",
-            "defense",
-            "magic",
-            "ranged",
-            "attackBonus",
-            "",
-            "",
-            "magicBonus",
-            "rangedBonus",
-            "strengthBonus",
-            "",
-            "",
-            "magicStrengthBonus",
-            "rangedStrengthBonus",
-            "stabDefense",
-            "slashDefense",
-            "crushDefense",
-            "magicDefense",
-            "rangedDefense",
-        ];
-
-        // Create text fields for the stat values
-        enemyStats.forEach((varName, index) => {
-            if (varName != "") {
-                let column = index % 5;
-                let row = Math.floor(index / 5);
-                this.enemyStatText[varName] = this.add.text(
-                    tableStartX + 55 + tableWidth * column,
-                    tableStartY + tableHeight * row,
-                    "0",
-                    FONTS.ITEM_STATS
-                );
-            }
-        });
-
-        this.hideObjectInfo();
+        this.show(false);
     }
 
-    hideObjectInfo() {
-        for (let varName in this.itemStatText) {
-            this.itemStatText[varName].visible = false;
-        }
-        this.itemStatLabels.forEach((label) => {
-            label.visible = false;
+    writeStrings(xDiff, ...textObjs) {
+        let textLine = [];
+        textObjs.forEach((textObj, index) => {
+            textLine.push(this.scrollWindow.add.text(index * xDiff, 0, textObj.text, textObj.format));
         });
-        for (let varName in this.enemyStatText) {
-            this.enemyStatText[varName].visible = false;
+        this.writeObjects(...textLine);
+    }
+
+    writeObjects(...objects) {
+        if (objects.length == 1) {
+            this.scrollWindow.addObject(objects[0]);
+            this.scrollWindow.refresh();
         }
-        this.enemyStatLabels.forEach((label) => {
-            label.visible = false;
-        });
-        this.objExamineText.visible = false;
-        this.objNameText.visible = false;
-        this.row2Header.visible = false;
-        this.row2Text.visible = false;
-        this.requiredLevelHeader.visible = false;
-        this.requiredLevelText.visible = false;
-        this.itemStatHeaders.visible = false;
-        this.enemyStatHeaders.visible = false;
-        this.chatWindow.visible = false;
-        this.shopChatWindow.visible = false;
-        this.welcomeText.visible = false;
-        this.playerNameText.visible = false;
+        else {
+            let row = new TextRow(this.scrollWindow, 0, 0, []);
+            objects.forEach(obj => {
+                row.add(obj);
+            });
+            console.log(row);
+            row.setX(0);
+            console.log("test");
+            this.scrollWindow.addObject(row);
+            this.scrollWindow.refresh();
+        }
+    }
+
+    writeEnemyInfo(enemy) {
+        this.writeStrings(
+            55, 
+            {text: "Stats:", format: FONTS.ITEM_HEADER},
+            {text: "Attack", format: FONTS.ITEM_STATS},
+            {text: enemy.attack, format: FONTS.ITEM_STATS},
+            {text: "Strength", format: FONTS.ITEM_STATS},
+            {text: enemy.strength, format: FONTS.ITEM_STATS},
+            {text: "Defense", format: FONTS.ITEM_STATS},
+            {text: enemy.defense, format: FONTS.ITEM_STATS},
+            {text: "Magic", format: FONTS.ITEM_STATS},
+            {text: enemy.magic, format: FONTS.ITEM_STATS},
+            {text: "Ranged", format: FONTS.ITEM_STATS},
+            {text: enemy.ranged, format: FONTS.ITEM_STATS},
+        );
+        this.writeStrings(
+            55, 
+            {text: "Accuracy Bonuses:", format: FONTS.ITEM_HEADER},
+            {text: "Melee", format: FONTS.ITEM_STATS},
+            {text: enemy.attackBonus, format: FONTS.ITEM_STATS},
+            {text: "", format: {}},
+            {text: "", format: {}},
+            {text: "", format: {}},
+            {text: "", format: {}},
+            {text: "Magic", format: FONTS.ITEM_STATS},
+            {text: enemy.magicBonus, format: FONTS.ITEM_STATS},
+            {text: "Ranged", format: FONTS.ITEM_STATS},
+            {text: enemy.rangedBonus, format: FONTS.ITEM_STATS},
+        );
+        this.writeStrings(
+            55, 
+            {text: "Damage Bonuses:", format: FONTS.ITEM_HEADER},
+            {text: "Melee", format: FONTS.ITEM_STATS},
+            {text: enemy.strengthBonus, format: FONTS.ITEM_STATS},
+            {text: "", format: {}},
+            {text: "", format: {}},
+            {text: "", format: {}},
+            {text: "", format: {}},
+            {text: "Magic", format: FONTS.ITEM_STATS},
+            {text: enemy.magicStrengthBonus, format: FONTS.ITEM_STATS},
+            {text: "Ranged", format: FONTS.ITEM_STATS},
+            {text: enemy.rangedStrengthBonus, format: FONTS.ITEM_STATS},
+        );
+        this.writeStrings(
+            55, 
+            {text: "Defense Bonuses:", format: FONTS.ITEM_HEADER},
+            {text: "Stab", format: FONTS.ITEM_STATS},
+            {text: enemy.stabDefense, format: FONTS.ITEM_STATS},
+            {text: "Slash", format: FONTS.ITEM_STATS},
+            {text: enemy.slashDefense, format: FONTS.ITEM_STATS},
+            {text: "Crush", format: FONTS.ITEM_STATS},
+            {text: enemy.crushDefense, format: FONTS.ITEM_STATS},
+            {text: "Magic", format: FONTS.ITEM_STATS},
+            {text: enemy.magicDefense, format: FONTS.ITEM_STATS},
+            {text: "Ranged", format: FONTS.ITEM_STATS},
+            {text: enemy.rangedDefense, format: FONTS.ITEM_STATS},
+        );
+    }
+
+    writeEquipmentInfo(equipment) {
+        this.writeStrings(0, {text: "Sells for: " + equipment.cost, format: FONTS.ITEM_STATS});
+        this.writeStrings(0, {text: "Required Level: " + equipment.requiredLevel, format: FONTS.ITEM_HEADER});
+        this.writeStrings(
+            55, 
+            {text: "Accuracy Bonuses:", format: FONTS.ITEM_HEADER},
+            {text: "Stab", format: FONTS.ITEM_STATS},
+            {text: equipment.stabBonus, format: FONTS.ITEM_STATS},
+            {text: "Slash", format: FONTS.ITEM_STATS},
+            {text: equipment.slashBonus, format: FONTS.ITEM_STATS},
+            {text: "Crush", format: FONTS.ITEM_STATS},
+            {text: equipment.crushBonus, format: FONTS.ITEM_STATS},
+            {text: "Magic", format: FONTS.ITEM_STATS},
+            {text: equipment.magicBonus, format: FONTS.ITEM_STATS},
+            {text: "Ranged", format: FONTS.ITEM_STATS},
+            {text: equipment.rangedBonus, format: FONTS.ITEM_STATS},
+        );
+        this.writeStrings(
+            55, 
+            {text: "Defense Bonuses:", format: FONTS.ITEM_HEADER},
+            {text: "Stab", format: FONTS.ITEM_STATS},
+            {text: equipment.stabDefenseBonus, format: FONTS.ITEM_STATS},
+            {text: "Slash", format: FONTS.ITEM_STATS},
+            {text: equipment.slashDefenseBonus, format: FONTS.ITEM_STATS},
+            {text: "Crush", format: FONTS.ITEM_STATS},
+            {text: equipment.crushDefenseBonus, format: FONTS.ITEM_STATS},
+            {text: "Magic", format: FONTS.ITEM_STATS},
+            {text: equipment.magicDefenseBonus, format: FONTS.ITEM_STATS},
+            {text: "Ranged", format: FONTS.ITEM_STATS},
+            {text: equipment.rangedDefenseBonus, format: FONTS.ITEM_STATS},
+        );
+        this.writeStrings(
+            55, 
+            {text: "Damage Bonuses:", format: FONTS.ITEM_HEADER},
+            {text: "Melee", format: FONTS.ITEM_STATS},
+            {text: equipment.strengthBonus, format: FONTS.ITEM_STATS},
+            {text: "Prayer", format: FONTS.ITEM_STATS},
+            {text: equipment.prayerBonus, format: FONTS.ITEM_STATS},
+            {text: "", format: {}},
+            {text: "", format: {}},
+            {text: "Magic", format: FONTS.ITEM_STATS},
+            {text: equipment.magicStrengthBonus, format: FONTS.ITEM_STATS},
+            {text: "Ranged", format: FONTS.ITEM_STATS},
+            {text: equipment.rangedStrengthBonus, format: FONTS.ITEM_STATS},
+        );
+    }
+
+    show(isVisible = true) {
+        this.scrollWindow.setVisible(isVisible);
+        this.playerNameText.visible = isVisible;
+        this.chatWindow.visible = isVisible;
+        this.shopChatWindow.visible = isVisible;
     }
 
     // Show object info in chat window
     showObjectInfo(isVisible, object = false, isShop = false) {
-        // Hide all text
-        this.hideObjectInfo();
-
-        // Show name, description, and stats from object
         if (object && isVisible) {
-            // Show name and description for all
-            this.objNameText.text = object.name;
-            this.objNameText.visible = true;
-            this.objExamineText.text = object.examineText;
-            this.objExamineText.visible = true;
-            this.welcomeText.visible = true;
-            this.playerNameText.visible = true;
+            // Write name & description
+            this.writeStrings(0, {text: object.name, format: FONTS.ITEM_HEADER});
+            this.writeStrings(0, {text: object.examineText, format: FONTS.ITEM_STATS});
 
             // Load bigger window on shop scene
             if (isShop) {
@@ -279,63 +210,20 @@ export class ChatScene extends Phaser.Scene {
             // Show different things for different types of objects
             switch (object.objectType) {
                 case OBJECT_TYPES.EQUIPMENT:
-                    this.requiredLevelHeader.visible = true;
-                    this.requiredLevelText.visible = true;
-                    this.requiredLevelText.text = object.requiredLevel;
-
-                    // Headers
-                    this.itemStatHeaders.visible = true;
-
-                    // Labels
-                    this.itemStatLabels.forEach((label) => {
-                        label.visible = true;
-                    });
-
-                    // Stat values
-                    for (let varName in this.itemStatText) {
-                        this.itemStatText[varName].text = object[varName];
-                        this.itemStatText[varName].visible = true;
-                    }
+                    this.writeEquipmentInfo(object);
+                    break;
                 case OBJECT_TYPES.ITEM:
-                    // Get cost/sale price
-                    if (isShop) {
-                        this.row2Header.text = "Cost:";
-                        this.row2Text.text = object.cost + "gp";
-                    } else {
-                        this.row2Header.text = "Sells for:";
-                        this.row2Text.text = Math.floor(object.cost / 2) + "gp";
-                    }
-
-                    this.row2Header.visible = true;
-                    this.row2Text.visible = true;
+                    this.writeStrings(0, {text: "Sells for: " + object.cost, format: FONTS.ITEM_STATS});
                     break;
                 case OBJECT_TYPES.ENEMY:
-                    // Headers
-                    this.enemyStatHeaders.visible = true;
-
-                    // Labels
-                    this.enemyStatLabels.forEach((label) => {
-                        label.visible = true;
-                    });
-
-                    // Stat values
-                    for (let varName in this.enemyStatText) {
-                        this.enemyStatText[varName].text = object[varName];
-                        this.enemyStatText[varName].visible = true;
-                    }
-
-                    this.row2Header.text = "HP:";
-                    this.row2Text.text = object.maxHealth;
-                    this.row2Header.visible = true;
-                    this.row2Text.visible = true;
+                    this.writeEnemyInfo(object);
                     break;
                 case OBJECT_TYPES.AUTOCLICKER:
-                    this.row2Header.text = "Cost:";
-                    this.row2Text.text = object.cost + "gp";
-                    this.row2Header.visible = true;
-                    this.row2Text.visible = true;
+                    //this.writeItemInfo(object);
                     break;
             }
+        } else {
+            this.show(false);
         }
     }
 }
